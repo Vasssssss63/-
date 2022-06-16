3edc15186118e1a7e84a7f25a3d1eb8aa6b3873e
andre <andre@DESKTOP-DVDM8A3>
2022-06-16 09:25:50 +0400

    Добавьте файлы проекта.

---
+++ да/Autorization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace да
+{
+    public partial class Autorization : Form
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" & textBox2.Text == "123")
+            {
+                Form Form1 = new Form1();
+                Form1.ShowDialog();
+            }
+            if (textBox1.Text == "user" & textBox2.Text == "321")
+            {
+                Form Form2 = new Form2();
+                Form2.ShowDialog();
+            }
+            if (textBox1.Text == "" & textBox2.Text == "")
+            {
+                Form Form3 = new Form3();
+                Form3.ShowDialog();
+            }
+            else
+                MessageBox.Show("Не верный логин или пароль");
+        }
+    }
+}
---
+++ да/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace да
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int captchaLenght = 0;
+
+            textBox1.Clear();
+
+            for (int i = 0; i < captchaLenght; i++) ;
+            {
+                textBox1.Text = textBox1.Text + rnd.Next(111111, 999999);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == textBox2.Text)
+            {
+                Form Form4 = new Form4();
+                Form4.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Не верная капча");
+            }
+        }
+    }
+    
+}
